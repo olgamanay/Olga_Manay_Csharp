5e8e2ffe60757d823a3181001d268c5c645cf931
unknown <you@example.com>
2023-01-13 17:18:56 +0300

    Add Masha

---
+++ Example005_Name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Write user name ");
+string username = Console.ReadLine();
+
+if(username.ToLower() =="masha")
+{
+    Console.WriteLine("Wow, it is MASHA");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
