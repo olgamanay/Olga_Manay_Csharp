40108a2dce07dcf4472c0bc348561097444fbd66
unknown <you@example.com>
2023-01-13 19:50:23 +0300

    Add magic while

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -20,15 +20,15 @@ int x = xa, y = xb;
 
 int count = 0;
 
-while(count <10)
+while(count < 10)
 {
-    int what = new Random().Next(0, 3);
+    int what = new Random().Next(0, 3); /// 0 1 2
     if (what == 0)
     {
-        x = (x = xa) / 2;
+        x = (x + xa) / 2;
         y = (y + ya) / 2;
     }
-       if (what == 1)
+    if (what == 1)
     {
         x = (x + xb) / 2;
         y = (y + yb) / 2;
@@ -38,8 +38,9 @@ while(count <10)
         x = (x + xc) / 2;
         y = (y + yc) / 2;
     }
-}
-
 Console.SetCursorPosition(x, y);
 Console.WriteLine("+");
 count = count + 1;
+}
+
+
