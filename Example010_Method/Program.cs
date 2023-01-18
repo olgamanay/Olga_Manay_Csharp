336c10f37d7be56dcdc48928272cff6205d83228
unknown <you@example.com>
2023-01-18 08:58:07 +0300

    Add method array

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {1, 12, 31, 18, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+        // break - прерви
+
+    }
+    index++;
+}
