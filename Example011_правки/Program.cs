eb565b173aef3f7f9ae75dcfda4f4b016197041a
unknown <you@example.com>
2023-01-19 13:37:22 +0300

    Add 11 task more control

---
+++ Example011_правки/Program.cs
@@ -0,0 +1,52 @@
+﻿int[] Array = new int[10];
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection [index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[]col)
+{
+    int count = col.Length;
+    int index = 0;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int indexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+
+    }
+    return position;
+}
+
+int [] array = new int [10];
+
+FillArray(array);
+array[4] = 4;
+array[4] = 6;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = indexOf(array, 4);
+Console.WriteLine(pos);
