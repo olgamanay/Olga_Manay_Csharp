5b603ff96e7b9932e5273f0408e2c4710c3179bc
unknown <you@example.com>
2023-01-24 11:44:11 +0300

    Add control oshibok

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Author Manay");
+}
+// как вызывать метод1:
+//Method1();
+
+// Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//как вызвать метод2:
+//Method2(msg: "Message's text");
+
+void Method2_1(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method2_1("Text", 4);
+//Method2_1(msg: "Text", count: 4);
+Method2_1(count: 4, msg: "New text");
