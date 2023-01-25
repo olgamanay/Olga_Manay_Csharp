7535feddebda04b4c4dc25fbfde1ab7919604e47
unknown <you@example.com>
2023-01-25 17:49:26 +0300

    add array methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -116,21 +116,39 @@
 //Console.WriteLine(newText);
 //Console.WriteLine();
 
-int [] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+ int [] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 
-void PrintArray(int[] array)
-{  
-     int count = Array.Length;
+ void PrintArray(int[] array)
+ {  
+     int count = array.Length;
 
      for (int i = 0; i < count; i++)
         {
             Console.Write($"{array[i]} ");
         }
-        Console.WriteLine();
-}
+       Console.WriteLine();
+  }
 
 
 void SelectionSort(int[] array)
+{
+     for (int i = 0; i < array.Length - 1 ; i++)
+     {
+         int minPosition = i;
+         
+         for (int j = i + 1; j < array.Length; j++)
+             {
+              if(array[j] < array[minPosition]) minPosition = j;
+             }
+         
+         int temporary = array[i];
+         array[i] = array[minPosition];
+         array[minPosition] = temporary;
+     }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
 
 
 PrintArray(arr);
